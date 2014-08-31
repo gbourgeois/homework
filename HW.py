2e5ffcb1cccebd619669af1b78773ea5090e15f7
bourgeois <bourgeois>
2014-08-31 21:59:14 +0000

    the assignment

--- HW.py
+++ HW.py
@@ -1,3 +1,39 @@
 __author__ = 'bourgeois'
-Hello, I am at classroom.
+import math
+from math import pi
+
+
 print("I was taught Python3 course. Hello bourgeois")
+
+#CIRCLE
+#Perimeter of a Circle
+def circle_perimeter(radius):
+    '''
+    Calculate perimeter of a circle from radius
+    :param radius: the radius
+    :return: the perimeter (same units as the radius)
+    '''
+    return 2*pi*radius
+
+print("The perimeter of my circle is ", circle_perimeter(10))
+
+#Area of a Circle
+def circle_area(radius):
+    return pi*radius*radius
+    '''
+    Calculate area of a circle from radius
+    :param radius: the radius
+    :return: the perimeter (units^2 from radius)
+    '''
+
+print("The area of my circle is ", circle_area(10))
+
+#PARALLELOGRAM
+#Perimeter of a parallelogram
+def parallelogram_perimeter(side1,side2):
+    return 2*(side1+side2)
+print (parallelogram_perimeter(2,4))
+#Area of Parallelogram
+def parallelogram_area(base,height):
+    return base*height
+print (parallelogram_area(3,5))
